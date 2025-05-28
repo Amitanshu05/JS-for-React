657d9f4fb93d545e8179aed73e6e524ca31774e6
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-05-28 21:13:42 +0530

    revised ternary operator

---
+++ terneryOperators.js
@@ -0,0 +1,20 @@
+let age=10;
+let name ="Krsna";
+
+if(age<10){
+    console.log(name="krsna");
+}else{
+   console.log(name="KR $");
+}
+
+//usage of ternary operators with above example
+
+const Username = age>10 ? "krishna" : "subhash"; // ? implies IF & : implies ELSE 
+console.log(Username);
+
+
+
+// const RandomFunction=()=>{
+//     return age>10 ? <div>Emiway</div>: <div>Raftaar</div>;
+// }
+// RandomFunction();
